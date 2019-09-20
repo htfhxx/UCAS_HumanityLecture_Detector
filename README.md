49179a3cc13d8e209a34701b81a2bd01d1eb29c1
xiao <496813470@qq.com>
2019-09-20 21:12:59 +0800

    add main.py

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # UCAS_HumanityLecture_Detector
 国科大人文讲座监测及自动报名脚本
+
+## 环境依赖
+
+python 3.6
+
+```sh
+pip install -r requirements.txt
+```
+
+
+## 使用
+修改配置文件 `config`, 格式如下
+```yaml
+# sep账号
+username: xiaoming@163.com
+# sep密码
+password: mimamima
+
+# 是否自动报名
+autoChoose: False
+
+# 查询间隔，单位 秒
+interval: 180
+
+# QQ邮箱及其授权码（QQ邮箱-账户-获取授权码）, 注意不是密码
+main_mail: 1111111@qq.com
+main_pwd: asdsadwqeqweqwee
+
+# 需要提醒的邮箱, 按对应格式继续添加即可
+otherMails:
+    - 1111111@qq.com
+    - 2222222@gmail.com
+```
+运行程序
+```python
+python main.py
+```
+
+## 问题
+
+Q: Linux运行程序时smtp超时无法连接?
+A: 修改`main.py` 32-33行:
+```python
+    self.smtpObj = smtplib.SMTP_SSL() 
+    self.smtpObj.connect(self.mail_host, 465)
+```
---
+++ main.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+import codecs
+import json
+import os
+import time
+from sys import exit
+import requests
+import re
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+import yaml
+       
+class UserNameOrPasswordError(Exception):
+    pass
+
+class MailSender:
+
+    def __init__(self, mail_user, mail_pass, destination_list, mail_host="smtp.qq.com"):
+
+        self.mail_host = mail_host
+        self.mail_user = mail_user
+        self.mail_pass = mail_pass
+        self.destination_list = destination_list
+
+        if self.mail_user not in self.destination_list:
+            self.destination_list.append(self.mail_user)
+        self.getConnect()
+    
+    def getConnect(self):
+        try:
+            self.smtpObj = smtplib.SMTP() 
+            self.smtpObj.connect(self.mail_host, 25)    # 25 为 SMTP 端口号
+            self.smtpObj.login(self.mail_user,self.mail_pass)
+        except Exception:
+            print("无法登录邮箱, 请检查用户名或密码是否正确")
+            exit(1)
+            
+
+    def content(self, passage):
+        message = MIMEText(passage, 'plain', 'utf-8')
+        message['From'] = Header("人文讲座提醒", "utf-8")
+        message['To'] =  Header("人文讲座", 'utf-8')
+        subject = '人文讲座提醒'
+        message['Subject'] = Header(subject, 'utf-8')
+        return message
+        
+
+    def send(self, passage):
+        for mail in self.destination_list:
+            message = self.content(passage)
+            try:
+                self.smtpObj.sendmail(self.mail_user, mail, message.as_string())
+                print ("成功发送至 ", mail)
+            except smtplib.SMTPException:
+                print ("无法发送邮件至 ", mail)
+
+
+class LoginUCAS(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.session = requests.session()
+        
+        self.cnt = 0
+        self.old_list = []
+        self.mail_sender = MailSender(config["main_mail"], config["main_pwd"], config["otherMails"])
+
+        self.url = {
+            'base_url': 'http://onestop.ucas.ac.cn/home/index',
+            'verification_code': None,
+            'login_url': 'http://onestop.ucas.ac.cn/Ajax/Login/0'
+        }
+        # self.session.get(self.url['base_url'])
+        self.headers = {
+            'Host': 'onestop.ucas.ac.cn',
+            "Connection": "keep-alive",
+            'Referer': 'http://onestop.ucas.ac.cn/home/index',
+            'X-Requested-With': 'XMLHttpRequest',
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
+        }
+        self.post_data = {
+            "username": self.config["username"],
+            "password": self.config["password"],
+            "remember": 'checked',
+        }
+
+    def parser(self):
+        url = 'http://jwxk.ucas.ac.cn/subject/humanityLecture'
+        r = self.session.get(url, headers=self.headers)
+        course_list = set(re.findall(r"<a href=\"#\" onclick=\"toSign\('([0-9]*)','(.*)'\)\">报名</a>", r.text))
+        return course_list
+
+
+    def check(self):
+        count = 0
+        while True:
+            count += 1
+            print("%d 次刷新..."%count)
+            try:
+                result = self.parser()
+                if len(result) > len(self.old_list):
+                    new_courses = result - self.old_list
+                    print("New %d Courses!"%(len(new_courses)))
+                    self.mail_sender.send("已发布%d门新讲座!"%(len(new_courses)))
+                    self.old_list = result
+                    if self.config["autoChoose"]:
+                        self.sign(new_courses)
+                time.sleep(self.config["interval"])
+            except requests.exceptions.ConnectionError:
+                print("网络错误, 正在重新连接...")
+                self.cnt += 1
+                if self.cnt > 20:
+                    print("教务处似乎登不上...")
+                    exit(1)
+                return self.login_sep()
+        
+    def sign(self, new_courses):
+        for info in new_courses:
+            data = {
+                "lectureId": info[0],
+                "communicationAddress": info[1]
+            }
+            r = self.session.post("http://jwxk.ucas.ac.cn/subject/toSign", data=data, headers=self.headers)
+            if r.text == "success":
+                print("讲座报名成功!")
+            else:
+                print("讲座报名失败!")
+
+    def login_sep(self):
+        try:
+            if not self.cnt:
+                print('Login....' + self.url['base_url'])
+            html = self.session.post(
+                self.url['login_url'], data=self.post_data, headers=self.headers).text
+            
+            res = json.loads(html)
+            if not res['f']:
+                raise UserNameOrPasswordError
+            else:
+                html = self.session.get(res['msg']).text
+                print("登录成功")
+            
+            url = "http://sep.ucas.ac.cn/portal/site/226/821"
+            r = self.session.get(url, headers=self.headers)
+            
+            try:
+                code = re.findall(r'"http://jwxk.ucas.ac.cn/login\?Identity=(.*)"', r.text)[0]
+            except IndexError:
+                raise NoLoginError
+
+            url = "http://jwxk.ucas.ac.cn/login?Identity=" + code
+            self.headers['Host'] = "jwxk.ucas.ac.cn"
+            self.session.get(url, headers=self.headers)
+            self.old_list = self.parser()
+            
+            print("当前可报名讲座数: %d"%(len(self.old_list)))
+            self.cnt = 0
+            
+        except UserNameOrPasswordError:
+            print('用户名或者密码错误')
+            exit(1)
+        except requests.exceptions.ConnectionError:
+            self.cnt += 1
+            if self.cnt > 20:
+                self.cnt = 0
+                print("教务处好像登不上...")
+            return self.login_sep()
+        return self
+
+
+if __name__ == '__main__':
+
+    with open("config", "r", encoding="utf8") as f:
+        config = yaml.load(f.read())
+    test = LoginUCAS(config)
+    test.login_sep()
+    test.check()
